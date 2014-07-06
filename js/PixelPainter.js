8281fb0261904d35cfe48a8bb1198aff334c4032
Gitthere <ichi4g@gmail.com>
2014-07-05 18:31:06 -1000

    added notes to js functions

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {//Specify a function to execute when the DOM is fully loaded.
   var holdColor;
   var td = $('tr.pallette > td');//gets direct descendent (td) of tr to get color. if use just .pallette, color would not register
-  
   td.click(function() {
-    holdColor = $(this).css("background-color");
+    holdColor = $(this).css("background-color");//transfers color of clicked td to holdColor
   });
 
   
-  var tdGrid = $('.grid td'); 
+  var tdGrid = $('.grid td');//gets descendent of td
   tdGrid.click(function() {
-    console.log("paint");
-    $(this).css("background-color", holdColor);
+    $(this).css("background-color", holdColor);//transfers color held in holdColor to td clicked in grid
   });
 
 });
