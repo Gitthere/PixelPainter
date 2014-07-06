42b39601ff02265bdc94134723ac6b782f90f384
Gitthere <ichi4g@gmail.com>
2014-07-05 22:02:58 -1000

    preliminary js for creating pallette. need to complete grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,20 +1,33 @@
-$(document).ready(function() {//Specify a function to execute when the DOM is fully loaded.
-  var holdColor;
-  var td = $('tr.pallette > td');//gets direct descendent (td) of tr to get color. if use just .pallette, color would not register
-  td.click(function() {
-    holdColor = $(this).css("background-color");//transfers color of clicked td to holdColor
-  });
+// $(document).ready(function() {//Specify a function to execute when the DOM is fully loaded.
+//   var holdColor;
+//   var td = $('tr.pallette > td');//gets direct descendent (td) of tr to get color. if use just .pallette, color would not register
+//   td.click(function() {
+//     holdColor = $(this).css("background-color");//transfers color of clicked td to holdColor
+//   });
 
   
-  var tdGrid = $('.grid td');//gets descendent of td
-  tdGrid.click(function() {
-    $(this).css("background-color", holdColor);//transfers color held in holdColor to td clicked in grid
-  });
+//   var tdGrid = $('.grid td');//gets descendent of td
+//   tdGrid.click(function() {
+//     $(this).css("background-color", holdColor);//transfers color held in holdColor to td clicked in grid
+//   });
 
+// });
+
+
+
+$(document).ready(function() {
+
+  var pallette = "";
+  var table = $('<table></table>');
+
+  for (var i = 0; i <= 6; i++) {
+    //$('#controls').append('<tr></tr>');
+    var tableRow = $('<tr/>');
+    for (var j = 0; j <= 6; j++)  {
+      $(tableRow).append('<td></td>');
+    }
+  //$('#controls').append(tableRow);
+  $(table).append(tableRow);
+  }
+  $('#controls').append(table);
 });
-
-
-
-// var pixelPainter = PixelPainter(20,20);
-// $("#controls").append(pixelPainter.controls);
-// $("#artboard").append(pixelPainter.artboard);
